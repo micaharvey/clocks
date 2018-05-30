364068136043354c372a4e54b558dd9e7bf23625
Micah Arvey <micaha@qualtrics.com>
2018-05-30 15:06:01 -0700

    new clocks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  date: Date;
+  dateTime: string;
+  constructor() {
+    this.updateTime();
+    setInterval(this.loop.bind(this), 1000);
+  }
+  loop() {
+    this.updateTime();
+    this.updateClock();
+  }
+  updateTime() {
+    this.date = new Date();
+    this.dateTime = this.date.toLocaleString();
+  }
+  updateClock() {
+    // get hours, update hand
+    var hours = this.date.getHours();
+    var hoursRotation = (360/12) * (hours%12);
+    document.getElementsByClassName("hour-hand")[0].setAttribute("style", "transform: rotateZ("+hoursRotation+"deg);");
+    // get minutes, update hand
+    var minutes = this.date.getMinutes();
+    var minutesRotation = (360/60) * minutes;
+    document.getElementsByClassName("minute-hand")[0].setAttribute("style", "transform: rotateZ("+minutesRotation+"deg);");
+  }
 }
